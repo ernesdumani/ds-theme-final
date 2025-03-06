e104e235567fbfd46917e1be35c8348719f5e811
dalmat-shabani <dalmat.shabani90@gmail.com>
2025-03-06 19:11:49 +0100

    a

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, repellat.</p>
+
+<header>
+    <section class="top-bar">
+        <div class="logo">
+            Logo
+        </div>
+        <div class="searchbox">
+            Search Box
+        </div>
+    </section>    
+    <section class="menu-area">
+        <nav class="main-menu">
+            Menu
+        </nav>
+    </section>
+</header>
